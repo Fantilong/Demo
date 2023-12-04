a3fc5859eac4345a4593fdb5afd15f1a84ab4f3a
fantilong <fantilong@wedobest.com.cn>
2023-12-04 21:06:37 +0800

    promise

--- JS异步/promise.js
+++ JS异步/promise.js
@@ -41,55 +41,62 @@
 //   return p
 // }
 
-// // const url = 'https://img.mukewang.com/5a9fc8070001a82402060220-140-140.jpg'
-// // loadImg(url).then(img => {
-// //     console.log(img.width)
-// //     return img
-// // }).then(img => {
-// //     console.log(img.height)
-// // }).catch(ex => console.error(ex))
 
-// const url1 = 'https://img.mukewang.com/5a9fc8070001a82402060220-140-140.jpg'
-// const url2 = 'https://img3.mukewang.com/5a9fc8070001a82402060220-100-100.jpg'
+// function loadImg(src) {
+//   return new Promise((resolve, reject) => {
+//     const img = document.createElement('img')
 
-// loadImg(url1).then(img1 => {
-//   console.log(img1.width)
-//   return img1 // 普通对象
-// }).then(img1 => {
-//   console.log(img1.height)
-//   return loadImg(url2) // promise 实例
-// }).then(img2 => {
-//   console.log(img2.width)
-//   return img2
-// }).then(img2 => {
-//   console.log(img2.height)
-// }).catch(ex => console.error(ex))
+//     img.onload = () => {
+//       resolve(img)
+//     }
+//     img.onerror = () => {
+//       reject(new Error(`图片加载失败 ${src}`))
+//     }
+
+//     img.src = src
+//   })
+// }
+
+// const url = 'https://seccdn.libravatar.org/avatar/a00cb054d121654bd0fba4bd5b166cb5?s=46&d=identicon'
+
+// async function appendImg() {
+//   try {
+//     const img = await loadImg(url)
+//     document.querySelector('body').appendChild(img)
+//   } catch (error) {
+//     alert(error.message)
+//   }
+// }
+// appendImg()
+
+/**
+ * then / catch 返回fulfilled状态的promise，如果里面有报错则返回rejected状态的promise
+ */
+// const p = Promise.resolve().then()
+// console.log(p); // fulfilled
+
+// const p1 = Promise.resolve().then(() => {
+//   throw new Error()
+// })
+// console.log(p1);// rejected
 
 
-function loadImg(src) {
-  return new Promise((resolve, reject) => {
-    const img = document.createElement('img')
+// resolve 修改promise状态为 fulfilled， 触发 then 当中的回调
+// reject 修改promise状态为 rejeted, 触发 catch 当中的回调
 
-    img.onload = () => {
-      resolve(img)
-    }
-    img.onerror = () => {
-      reject(new Error(`图片加载失败 ${src}`))
-    }
+// then 和 catch 都返回一个 fulfilled 状态的promise,如果当中有报错，则返回 rejected 的promise
+// rejected 状态的promise 则触发 catch，fullfilled 状态的 promise则触发 then
 
-    img.src = src
-  })
-}
 
-const url = 'https://seccdn.libravatar.org/avatar/a00cb054d121654bd0fba4bd5b166cb5?s=46&d=identicon'
+const p = new Promise((resolve, reject) => {
+  throw new Error(123)
+  resolve(123)
+})
+console.log(p);
+const p1 = new Promise((resolve, reject) => {
+  resolve(123)
+}).then(() => {
+  throw new Error(123)
+})
+console.log(p1);
 
-async function appendImg() {
-  try {
-    const img = await loadImg(url)
-    document.querySelector('body').appendChild(img)
-  } catch (error) {
-    alert(error.message)
-  }
-}
-
-appendImg()
